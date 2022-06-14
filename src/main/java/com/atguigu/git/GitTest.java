62b5f7fc35da11a8ed736d3f941155260134b80d
wjq <1076727225@qq.com>
2022-06-14 17:55:25 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @creat 2022 - 06 - 14 - 17:50
+ * @Description
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello git!");
+    }
+}
