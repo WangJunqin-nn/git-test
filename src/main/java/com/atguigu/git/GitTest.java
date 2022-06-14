2cd5ea517acb66f96106c634da80301aaa948174
wjq <1076727225@qq.com>
2022-06-14 20:01:16 +0800

    push commit1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("Hello git hot-fix!");
         System.out.println("Hello git master!");
         System.out.println("Hello git hot-fix2!");
+        System.out.println("push test");
     }
 }
